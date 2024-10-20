af831e216d960f7c5b189072d0b2732e1d7d36b2
Michel Hamelink <info@michelhamelink.nl>
2024-10-20 15:02:57 +0200

    feat: button design

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,26 +2,22 @@ import React from 'react';
 
 interface ButtonProps {
     size?: 'small' | 'medium' | 'large';
-    color?: string; // Tailwind colors
+    color?: 'primary';
     children: React.ReactNode;
     onClick?: () => void;
 }
 
-const Button = ({size = 'medium', color = 'purple', children, onClick}: ButtonProps) => {
+const Button = ({color = 'primary', size = 'medium', children, onClick}: ButtonProps) => {
     const sizeClasses = size === 'small' ? 'px-3 py-2 text-sm' : 'px-6 py-3 text-lg';
 
     const colorClasses = {
-        primary: '  hover:bg-violet-500',
-        secondary: 'bg-gray-600 text-white hover:bg-gray-700',
-        danger: 'bg-red-600 text-white hover:bg-red-700',
-        success: 'bg-green-600 text-white hover:bg-green-700',
+        primary: 'bg-gray-800 hover:bg-gray-700  border-gray-600 hover:border-gray-400 focus:border-gray-400',
     }[color];
-    
+
     return (
         <button
             onClick={onClick}
-            className={`rounded-md ${sizeClasses} ${colorClasses} bg-violet-600 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-${color}-500`}
-
+            className={`${sizeClasses} ${colorClasses} border-[1px] rounded-[4px] font-medium tracking-wide transition-all duration-300 focus:outline-none flex items-center align-middle px-4`}
         >
             {children}
         </button>
--- src/pages/Lofanje/index.tsx
+++ src/pages/Lofanje/index.tsx
@@ -3,6 +3,7 @@ import CategorySelect from "./CategorySelect";
 import LanguageSelect from "./LanguageSelect";
 import {Option} from "../../components/form/Select";
 import Button from "../../components/ui/Button";
+import {FaPlus} from "react-icons/fa6";
 
 const Lofanje = () => {
 
@@ -26,10 +27,11 @@ const Lofanje = () => {
             <main>
                 {category && (
                     <section className="container mx-auto mt-5 bg-gray-900 p-5 shadow-lg text-white rounded-md">
-                        <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-between mb-4">
                             <h2 className="text-xl font-bold">{language?.label} | {category.label}</h2>
-                            <Button color="primary" size="small">new entry</Button>
+                            <Button color="primary" size="small"><FaPlus className="mr-2"/> new entry</Button>
                         </div>
+
                     </section>
                 )}
             </main>
