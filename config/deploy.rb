8a792750f53fb0033d91fd43faf5e425dce65d9e
Michel Hamelink <info@michelhamelink.nl>
2024-11-10 10:17:41 +0100

    chore: updated deploy rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,12 +7,15 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # Keep the last 5 releases
 set :keep_releases, 5
 
+# Custom NVM Cache Directory
+set :nvm_cache_dir, "/home/lofanje/.nvm-cache"
+
 namespace :deploy do
   desc 'Install correct Node.js version from .nvmrc'
   task :load_nvm do
     on roles(:app) do
       within release_path do
-        execute "cd #{release_path} && source $HOME/.bashrc && nvm install"
+        execute "mkdir -p #{fetch(:nvm_cache_dir)} && export NVM_CACHE_DIR=#{fetch(:nvm_cache_dir)} && source $HOME/.bashrc && cd #{release_path} && nvm install"
       end
     end
   end
@@ -21,7 +24,7 @@ namespace :deploy do
   task :npm_install do
     on roles(:app) do
       within release_path do
-        execute "cd #{release_path} && source $HOME/.bashrc && nvm use && npm install"
+        execute "export NVM_CACHE_DIR=#{fetch(:nvm_cache_dir)} && source $HOME/.bashrc && cd #{release_path} && nvm use && npm install"
       end
     end
   end
@@ -30,7 +33,7 @@ namespace :deploy do
   task :npm_run_build do
     on roles(:app) do
       within release_path do
-        execute "cd #{release_path} && source $HOME/.bashrc && nvm use && npm run build"
+        execute "export NVM_CACHE_DIR=#{fetch(:nvm_cache_dir)} && source $HOME/.bashrc && cd #{release_path} && nvm use && npm run build"
       end
     end
   end
